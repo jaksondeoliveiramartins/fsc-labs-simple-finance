3c2339c3328bf3ee8fc0b57d15ec9733c9b7d36e
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-15 19:13:45 -0300

    feat: add components to improve project

---
+++ app/_components/OtherLoginMethods.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import { Button } from "../_lib/components/ui/button";
+import Image from "next/image";
+
+export function OtherLoginMethods() {
+  return (
+    <div className="flex w-full flex-col gap-10">
+      <div className="flex w-full items-center text-xs text-gray-400 uppercase">
+        <div className="flex-grow border-t border-gray-600" />
+        <span className="px-4">ou continue com</span>
+        <div className="flex-grow border-t border-gray-600" />
+      </div>
+
+      <div className="flex justify-center space-x-4">
+        <Button
+          type="button"
+          variant="outline"
+          className="h-9 w-9 rounded-full border-0 bg-gradient-to-b from-[#18acfe] to-[#0163e0] p-0"
+          asChild
+        >
+          <Link
+            href="/auth/facebook"
+            className="flex items-center justify-center"
+          >
+            <Image
+              src="/images/facebook.svg"
+              alt="Facebook"
+              width={12}
+              height={12}
+            />
+          </Link>
+        </Button>
+
+        <Button
+          type="button"
+          variant="outline"
+          className="h-9 w-9 rounded-full border-0 bg-[#283544] p-0 hover:bg-[#283544]"
+          asChild
+        >
+          <Link href="/auth/apple" className="flex items-center justify-center">
+            <Image src="/images/apple.svg" alt="Apple" width={16} height={16} />
+          </Link>
+        </Button>
+
+        <Button
+          type="button"
+          variant="outline"
+          className="rounded-full border-0 bg-[#141414] p-0 hover:bg-[#141414]"
+          asChild
+        >
+          <Link href="/auth/google">
+            <Image
+              src="/images/google.svg"
+              alt="Google"
+              width={32}
+              height={32}
+            />
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import { Eye, EyeOff } from "lucide-react";
-import { Input } from "../_lib/components/ui/input";
-import { Button } from "../_lib/components/ui/button";
 import Logo from "../_components/logo";
+import { OtherLoginMethods } from "../_components/OtherLoginMethods";
+import { PasswordInput } from "../_components/password-input";
+import { Button } from "../_lib/components/ui/button";
+import { Input } from "../_lib/components/ui/input";
 
 export default function SignUp() {
-  const [showPassword, setShowPassword] = useState(false);
-
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="relative mb-[50px]">
+    <main className="flex h-full flex-col items-center space-y-12">
+      <div className="relative space-y-12">
         <Logo className="mb-[50px]" />
 
-        <h1 className="mb-[30px] h-[26px] text-[24px] font-semibold text-white">
+        <h1 className="mb-[30px] h-7 text-2xl font-semibold text-white">
           Entrar na sua conta
         </h1>
         <p className="text-sm font-normal">
@@ -30,103 +27,29 @@ export default function SignUp() {
         </p>
       </div>
 
-      <form className="mb-[50px] w-full flex-1 space-y-[20px]">
-        <div className="relative">
-          <Input
-            id="email"
-            type="email"
-            required
-            className="h-[62px] w-full rounded-[12px] border-0 bg-white px-[22px] text-black focus:border-0 focus:ring-0 focus:outline-none"
-            placeholder="Email"
-          />
-        </div>
-
-        <div className="relative">
-          <Input
-            id="password"
-            type={showPassword ? "text" : "password"}
-            required
-            className="h-[62px] w-full rounded-[12px] border-0 bg-white px-[22px] pr-12 text-black focus:border-0 focus:ring-0 focus:outline-none"
-            placeholder="Senha"
-          />
-          <button
-            type="button"
-            onClick={() => setShowPassword(!showPassword)}
-            className="absolute top-1/2 right-[22px] -translate-y-1/2 text-gray-500 hover:text-gray-700"
-          >
-            {showPassword ? (
-              <Eye className="h-4 w-4" />
-            ) : (
-              <EyeOff className="h-4 w-4" />
-            )}
-          </button>
-        </div>
-
-        <Button
-          type="submit"
-          className="h-[62px] w-full cursor-pointer rounded-[12px] bg-[#55D462] text-white transition-colors duration-200 hover:bg-green-700"
-        >
-          Entrar
-        </Button>
-      </form>
-
-      <div className="relative mb-[50px] w-full">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t border-gray-500" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-[#141414] px-5 text-gray-400">
-            ou continue com
-          </span>
-        </div>
-      </div>
-
-      <div className="mb-10 flex justify-center space-x-4 pb-8">
-        <Button
-          type="button"
-          variant="outline"
-          className="h-9 w-9 rounded-full border-0 bg-gradient-to-b from-[#18acfe] to-[#0163e0] p-0"
-          asChild
-        >
-          <Link
-            href="/auth/facebook"
-            className="flex items-center justify-center"
-          >
-            <Image
-              src="/images/facebook.svg"
-              alt="Facebook"
-              width={12}
-              height={12}
+      <div className="flex min-h-[500px] flex-col justify-between">
+        <form className="w-full flex-1 space-y-5">
+          <div className="relative">
+            <Input
+              id="email"
+              type="email"
+              required
+              className="h-[62px] w-full rounded-[12px] border-0 bg-white px-[22px] text-black focus:border-0 focus:ring-0 focus:outline-none"
+              placeholder="Email"
             />
-          </Link>
-        </Button>
+          </div>
 
-        <Button
-          type="button"
-          variant="outline"
-          className="h-9 w-9 rounded-full border-0 bg-[#283544] p-0 hover:bg-[#283544]"
-          asChild
-        >
-          <Link href="/auth/apple" className="flex items-center justify-center">
-            <Image src="/images/apple.svg" alt="Apple" width={16} height={16} />
-          </Link>
-        </Button>
+          <PasswordInput />
 
-        <Button
-          type="button"
-          variant="outline"
-          className="rounded-full border-0 bg-[#141414] p-0 hover:bg-[#141414]"
-          asChild
-        >
-          <Link href="/auth/google">
-            <Image
-              src="/images/google.svg"
-              alt="Google"
-              width={32}
-              height={32}
-            />
-          </Link>
-        </Button>
+          <Button
+            type="submit"
+            className="h-[62px] w-full cursor-pointer rounded-[12px] bg-[#55D462] text-white transition-colors duration-200 hover:bg-green-700"
+          >
+            Entrar
+          </Button>
+        </form>
+
+        <OtherLoginMethods />
       </div>
     </main>
   );
