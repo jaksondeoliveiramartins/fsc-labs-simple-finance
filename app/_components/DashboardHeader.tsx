7bfa43ae32fb275d1d1ed00c85bb468761619fc8
Fabricio Camargo <fmcfdev@outlook.com>
2025-04-28 14:06:33 -0300

    chore: replace <div> with semantic <header> and <nav> tags

--- app/_components/DashboardHeader.tsx
+++ app/_components/DashboardHeader.tsx
@@ -3,12 +3,12 @@ import Logo from "./Logo";
 
 const DashboardHeader = () => {
   return (
-    <div className="fixed top-0 z-50 flex h-[100px] w-[440px] items-center justify-between bg-black px-[30px]">
-      <div className="relative flex w-full items-center justify-between">
+    <header className="fixed top-0 z-50 flex h-[100px] w-[440px] items-center justify-between bg-black px-[30px]">
+      <nav className="relative flex w-full items-center justify-between">
         <Logo isDark={true} />
         <HamburgerMenu />
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 };
 
