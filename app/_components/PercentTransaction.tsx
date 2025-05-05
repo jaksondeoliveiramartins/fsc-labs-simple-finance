f15dd88b23e2284fa1eaa413a70e2f7e643293b9
Fabricio Camargo <fmcfdev@outlook.com>
2025-05-05 20:59:29 -0300

    refactor: percentual transaction refactoring

--- app/_components/PercentTransaction.tsx
+++ app/_components/PercentTransaction.tsx
@@ -1,23 +1,28 @@
 import { CircleIcon } from "lucide-react";
+import {
+  TransactionTypeConfig,
+  TransactionType,
+} from "../_types/transactionType";
 
 interface PercentTransactionProps {
-  backgroundColor: "green" | "blue" | "red";
-  transactionType: "ganhos" | "gastos" | "investimento";
+  transactionType: TransactionType;
   percentNumber: number;
 }
 
 const PercentTransaction = ({
-  backgroundColor,
   transactionType,
   percentNumber,
 }: PercentTransactionProps) => {
+  const { color, label } = TransactionTypeConfig[transactionType];
   return (
     <div className="flex w-1/2 items-center justify-between">
       <div className="flex items-center gap-2">
-        <CircleIcon size={17} color={backgroundColor} fill={backgroundColor} />
-        <p className="text-sm font-semibold text-gray-400">{transactionType}</p>
+        <CircleIcon size={17} color={`${color}`} fill={`${color}`} />
+        <p className="text-sm font-normal text-[var(--label-foreground)]">
+          {label}
+        </p>
       </div>
-      <p>{percentNumber}%</p>
+      <p className="text-sm">{percentNumber}%</p>
     </div>
   );
 };
