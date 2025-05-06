999e9a16108d3049176a1e0810f13a1278a69465
Eduardo <eduardomuller989@gmail.com>
2025-05-06 17:04:47 -0400

    feat: add Dialog component

--- app/_components/TransactionActions.tsx
+++ app/_components/TransactionActions.tsx
@@ -1,32 +1,30 @@
+"use client";
+
 import { Plus } from "lucide-react";
 import Image from "next/image";
 import externalLinkIcon from "@/public/images/external-link.svg";
+import AddDialog from "./AddDialog";
+import { useState } from "react";
+import { Dialog, DialogTrigger } from "../_lib/components/ui/dialog";
+import { Button } from "../_lib/components/ui/button";
 
-interface TransactionActionsProps {
-  addRoute?: string;
-  viewRoute?: string;
-}
-
-export default function TransactionActions({
-  addRoute = "/transaction/add",
-  viewRoute = "/transaction",
-}: TransactionActionsProps) {
+export default function TransactionActions() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="rounded-[12px] bg-[#242424] px-[30px] py-[35px]">
       <div className="flex items-center justify-between gap-[15px]">
         <h2 className="text-base font-semibold text-white">Transações</h2>
         <div className="flex gap-2">
-          <a
-            href={addRoute}
-            className="flex h-[30.29px] transform items-center gap-[3.74px] rounded-[5px] bg-neutral-700 px-[10px] text-xs text-white transition-transform hover:scale-105 hover:bg-neutral-600"
-          >
-            <Plus size={14} />
-            Adicionar
-          </a>
-          <a
-            href={viewRoute}
-            className="flex h-[30.29px] transform items-center justify-center rounded-[5px] bg-neutral-700 px-[10px] text-xs text-white transition-transform hover:scale-105 hover:bg-neutral-600"
-          >
+          <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger>
+              <Button className="flex h-[30.29px] transform items-center gap-[3.74px] rounded-[5px] bg-neutral-700 px-[10px] text-xs text-white transition-transform hover:scale-105 hover:bg-neutral-600">
+                <Plus size={14} />
+                Adicionar
+              </Button>
+            </DialogTrigger>
+            <AddDialog />
+          </Dialog>
+          <a className="flex h-[30.29px] transform items-center justify-center rounded-[5px] bg-neutral-700 px-[10px] text-xs text-white transition-transform hover:scale-105 hover:bg-neutral-600">
             <Image
               src={externalLinkIcon}
               alt="Visualizar Transações"
