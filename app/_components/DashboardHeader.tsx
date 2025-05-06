8438a5d60fdd481bef61a3b54867e6ab97b977cc
Fabricio Camargo <fmcfdev@outlook.com>
2025-05-05 22:19:25 -0300

    feat: add hide/show amount button

--- app/_components/DashboardHeader.tsx
+++ app/_components/DashboardHeader.tsx
@@ -1,12 +1,33 @@
+import { EyeClosedIcon, EyeIcon } from "lucide-react";
 import HamburgerMenu from "./HamburgerMenu";
 import Logo from "./Logo";
 
-const DashboardHeader = () => {
+interface DashboardHeaderProps {
+  isShowAmmount: boolean;
+  handleShowAmountAction: (showAmount: boolean) => void;
+}
+
+const DashboardHeader = ({
+  isShowAmmount,
+  handleShowAmountAction,
+}: DashboardHeaderProps) => {
   return (
     <header className="fixed top-0 z-50 flex h-[100px] w-[440px] items-center justify-between bg-black px-[30px]">
       <nav className="relative flex w-full items-center justify-between">
         <Logo isDark={true} />
-        <HamburgerMenu />
+        <div>
+          <div
+            className="mr-11 cursor-pointer"
+            onClick={() => handleShowAmountAction(!isShowAmmount)}
+          >
+            {isShowAmmount ? (
+              <EyeIcon stroke={`var(--primary)`} size={22} />
+            ) : (
+              <EyeClosedIcon stroke={`var(--primary)`} size={22} />
+            )}
+          </div>
+          <HamburgerMenu />
+        </div>
       </nav>
     </header>
   );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuthContext } from "../_contexts/auth";
 import { useRouter } from "next/navigation";
 import DashboardHeader from "../_components/DashboardHeader";
@@ -9,8 +9,13 @@ import BalanceCard from "../_components/BalanceCard";
 import TransactionActions from "../_components/TransactionActions";
 import LoadingSpinner from "../_components/LoadingSpinner";
 export default function Dashboard() {
+  const [isShowAmount, setShowAmount] = useState(false);
   const { user, isInitializing } = useAuthContext();
   const router = useRouter();
+
+  const handleShowAmount = (showAmount: boolean) => {
+    setShowAmount(showAmount);
+  };
 
   useEffect(() => {
     if (!isInitializing && !user) {
@@ -24,11 +29,14 @@ export default function Dashboard() {
 
   return (
     <div className="dash-container w-full">
-      <DashboardHeader />
+      <DashboardHeader
+        handleShowAmountAction={handleShowAmount}
+        isShowAmmount={isShowAmount}
+      />
       <div className="mt-[68px] flex w-full flex-col items-center">
         <div className="flex w-full items-center justify-between py-8">
           <div>
-            Olá <span className="font-semibold">{user.firstName}</span>!
+            Olá <span className="font-semibold">{user?.firstName}</span>!
           </div>
           <DateRangePicker handleOnClick={() => {}} />
         </div>
@@ -37,22 +45,26 @@ export default function Dashboard() {
             label="Saldo"
             amount={12500.5}
             image="/images/empty-wallet.svg"
+            showValue={isShowAmount}
           />
           <BalanceCard
             label="Ganhos"
             amount={330.5}
             image="/images/empty-earnings.svg"
+            showValue={isShowAmount}
           />
           <BalanceCard
             label="Gastos"
             amount={1500.75}
             image="/images/empty-expenses.svg"
+            showValue={isShowAmount}
           />
 
           <BalanceCard
             label="Investimentos"
             amount={5000.0}
             image="/images/empty-investment.svg"
+            showValue={isShowAmount}
           />
         </div>
 
